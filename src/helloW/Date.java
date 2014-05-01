49cdedb43f7132b920b98d2ea01719db30adcb20
funbee <funbee1@hotmail.com>
2014-05-01 03:02:44 +0100

    Testing Push

---
+++ src/helloW/Date.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package helloW;
+
+/**
+ * @author USER
+ *
+ */
+public class Date {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String day = ("Thursday");
+		String month = ("May");
+		int date = 1;
+		int year = 2014;
+		
+		System.out.println("European Format:");
+		System.out.print((day) + (" ") + (date) + (" ") + (month) +(", "));
+		System.out.println(year);
+		
+		System.out.println("American Format:");
+		System.out.print((day) + (",") + (month)+ " " + (date) +(", "));
+		System.out.println(year);
+		
+	}
+
+}
